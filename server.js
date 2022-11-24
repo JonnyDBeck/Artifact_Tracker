8d6b352d807d9564679e17760e7bbdc85b125fb3
Broly <vidovicp20@gmail.com>
2022-11-23 21:10:01 -0500

    Added functional photo carousel and database

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening at port 3001: http://localhost: 3001'));
+    app.listen(PORT, () => console.log('Now listening at port 3001: http://localhost:3001'));
 
 });
