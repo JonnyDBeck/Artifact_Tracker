daff2fe9b56971fc96869af3754f705349626ce5
Broly <vidovicp20@gmail.com>
2022-11-28 22:19:31 -0500

    Routing results page and search bar html/css updated

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -5,4 +5,14 @@ router.get('/', async (req, res) => {
     res.render('homepage') })
 
 
+
+router.get('/results', async (req, res) => { 
+    console.log("params", req.query)
+
+    const requestedLocation = req.query.location
+    
+
+
+    res.render('results') })
+    
 module.exports = router;
