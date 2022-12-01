7ed8b96b6fcc16183808c446a8a8f4680d8235d4
Nick Prater <NikPratr@gmail.com>
2022-12-01 18:13:08 -0500

    corrected search bar

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,18 +1,39 @@
+const { Resturant } = require('../models');
+
 const router = require('express').Router();
 
-router.get('/', async (req, res) => { 
+router.get('/', async (req, res) => {
 
-    res.render('homepage') })
+    res.render('homepage');
+
+});
 
 
 
-router.get('/results', async (req, res) => { 
+router.get('/results', async (req, res) => {
     console.log("params", req.query)
 
-    const requestedLocation = req.query.location
-    
+    try {
+        const restData = await Resturant.findAll({
+            where:
+            {
+                City: req.query.city,
+                State: req.query.state
+            },
+        });
 
+        const restaurant = restData.map(Restaurant => Restaurant.get({ plain: true }));
+        console.log(restaurant);
 
-    res.render('results') })
-    
+        res.render('results'
+            // , {
+            //   ...restaurant,
+            //   logged_in: req.session.logged_in
+            // }
+        );
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
