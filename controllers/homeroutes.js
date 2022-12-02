2ec20d08ab2ec6d09dc9dbdcbd30a986af604384
Jon Beck <110854519+JonnyDBeck@users.noreply.github.com>
2022-12-01 21:18:14 -0500

    Fixed merge error

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -36,35 +36,4 @@ router.get('/results', async (req, res) => {
     }
 });
 
-<<<<<<< HEAD
-    res.render('results') })
-
-    router.get('/results', async (req, res) => { 
-        console.log("params", req.query)
-    
-        try {
-            const restData = await Resturant.findByPk(req.params.id, {
-              where:
-                {
-                  city: req.query.city,
-                  state: req.query.state
-                },
-            });
-        
-            const restaurant = restData.get({ plain: true });
-            console.log(restaurant);
-        
-            res.render('results'
-            // , {
-            //   ...restaurant,
-            //   logged_in: req.session.logged_in
-            // }
-            );
-          } catch (err) {
-            res.status(500).json(err);
-        }
-    });
-    
-=======
->>>>>>> c3408c1dbd4bdc0a966330090b9300c7ada282a1
 module.exports = router;
