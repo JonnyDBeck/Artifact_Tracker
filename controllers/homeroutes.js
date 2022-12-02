13d8d402e7b8ca8e068baca212476a92551dc3d8
Broly <vidovicp20@gmail.com>
2022-12-01 19:31:30 -0500

    Added about page handlebars and css styling

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -14,5 +14,31 @@ router.get('/results', async (req, res) => {
 
 
     res.render('results') })
+
+    router.get('/results', async (req, res) => { 
+        console.log("params", req.query)
+    
+        try {
+            const restData = await Resturant.findByPk(req.params.id, {
+              where:
+                {
+                  city: req.query.city,
+                  state: req.query.state
+                },
+            });
+        
+            const restaurant = restData.get({ plain: true });
+            console.log(restaurant);
+        
+            res.render('results'
+            // , {
+            //   ...restaurant,
+            //   logged_in: req.session.logged_in
+            // }
+            );
+          } catch (err) {
+            res.status(500).json(err);
+        }
+    });
     
 module.exports = router;
