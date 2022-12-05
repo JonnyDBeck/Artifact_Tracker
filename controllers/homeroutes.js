4f872c4febbe396fbf1c5962a57808f6e98cc4a7
Nick Prater <NikPratr@gmail.com>
2022-12-04 23:08:40 -0500

    minor change

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Resturant, Foodie } = require('../models');
+const { Resturant } = require('../models');
 
 router.get('/', async (req, res) => { 
 
